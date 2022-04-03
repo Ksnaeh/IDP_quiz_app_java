5534156158283762799442fa12b780adc0ea3150
Chee Hean Chua <adamcheehean01@gmail.com>
2022-04-03 20:04:06 +0800

    TODO: design and bug fixes
    -Allow all 3 questions to be displayed
    
    Design:
    -auth and registration page
    -join session page
    -question and answer page
    -end of quiz page with total score

--- app/src/main/java/com/example/javaquizapp/MainActivity.java
+++ app/src/main/java/com/example/javaquizapp/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.javaquizapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
+//import androidx.preference.PreferenceManager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
