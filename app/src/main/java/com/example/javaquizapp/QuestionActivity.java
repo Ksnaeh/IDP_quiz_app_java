afe9f062f46a2856ace8df9a9968a722d1aafaf8
Ksnaeh <65495468+Ksnaeh@users.noreply.github.com>
2022-04-04 01:48:11 +0800

    completed design
    yayyy

--- app/src/main/java/com/example/javaquizapp/QuestionActivity.java
+++ app/src/main/java/com/example/javaquizapp/QuestionActivity.java
@@ -117,33 +117,33 @@ public class QuestionActivity extends AppCompatActivity {
                                 //todo: use 3000 for debugging (default 10000)
                                 cdt = new CountDownTimer(3000, 1000) {
                                     public void onTick(long millisRemaining) {
-                                        showQuestion.setText("Question: " + questions.get(startingindex-1));
+                                        showQuestion.setText("Question: " + questions.get(startingindex));
 
-                                        button_a.setText(choice_a.get(startingindex-1));
-                                        button_b.setText(choice_b.get(startingindex-1));
-                                        button_c.setText(choice_c.get(startingindex-1));
-                                        button_d.setText(choice_d.get(startingindex-1));
+                                        button_a.setText(choice_a.get(startingindex));
+                                        button_b.setText(choice_b.get(startingindex));
+                                        button_c.setText(choice_c.get(startingindex));
+                                        button_d.setText(choice_d.get(startingindex));
 
                                         if (isSelected == 0){
                                             button_a.setEnabled(true);
-                                            button_a.setBackgroundColor(Color.BLUE);
+                                            button_a.setBackgroundColor(Color.parseColor("#8FBFE0"));
 
 
                                             button_b.setEnabled(true);
-                                            button_b.setBackgroundColor(Color.BLUE);
+                                            button_b.setBackgroundColor(Color.parseColor("#8FBFE0"));
 
                                             button_c.setEnabled(true);
-                                            button_c.setBackgroundColor(Color.BLUE);
+                                            button_c.setBackgroundColor(Color.parseColor("#8FBFE0"));
 
                                             button_d.setEnabled(true);
-                                            button_d.setBackgroundColor(Color.BLUE);
+                                            button_d.setBackgroundColor(Color.parseColor("#8FBFE0"));
                                         }
 
                                     }
                                     public void onFinish() {
                                         showQuestion.setText("End of Quiz!");
                                         //onTick(10000);
-                                        //addResponse();
+                                        countOnce = 0;
                                         if (isSelected == 0){
                                             endQuestion();
                                         }
@@ -170,7 +170,7 @@ public class QuestionActivity extends AppCompatActivity {
                                 };
 
                                 cdt.start();
-                                startingindex = 1;
+                                startingindex = 0;
 
                             }
                         } else {
@@ -229,33 +229,33 @@ public class QuestionActivity extends AppCompatActivity {
                     showQuestion.setText("End of Question Results");
                 }
 
-                if (button_a.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_a.getText().toString().equals(answers.get(startingindex))){
                     button_a.setClickable(false);
-                    button_a.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_a.setBackgroundColor(Color.parseColor("#A8C256"));
                 }
                 else {
                     button_a.setBackgroundColor(Color.LTGRAY);
                     button_a.setEnabled(false);
                 }
-                if (button_b.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_b.getText().toString().equals(answers.get(startingindex))){
                     button_b.setClickable(false);
-                    button_b.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_b.setBackgroundColor(Color.parseColor("#A8C256"));
                 }
                 else {
                     button_b.setBackgroundColor(Color.LTGRAY);
                     button_b.setEnabled(false);
                 }
-                if (button_c.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_c.getText().toString().equals(answers.get(startingindex))){
                     button_c.setClickable(false);
-                    button_c.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_c.setBackgroundColor(Color.parseColor("#A8C256"));
                 }
                 else {
                     button_c.setBackgroundColor(Color.LTGRAY);
                     button_c.setEnabled(false);
                 }
-                if (button_d.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_d.getText().toString().equals(answers.get(startingindex))){
                     button_d.setClickable(false);
-                    button_d.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_d.setBackgroundColor(Color.parseColor("#A8C256"));
                 }
                 else {
                     button_d.setBackgroundColor(Color.LTGRAY);
@@ -264,30 +264,38 @@ public class QuestionActivity extends AppCompatActivity {
             }
             public void onFinish() {
                 if (startingindex < iterations){
-                    startingindex++;
-                    cdt.start();
+                    if (countOnce == 0){
+                        startingindex++;
+                        countOnce = 1;
 
-                    button_a.setEnabled(true);
-                    //button_a.setBackgroundColor(0xFF673AB7);
+                        button_a.setEnabled(true);
+                        //button_a.setBackgroundColor(0xFF673AB7);
 
-                    button_b.setEnabled(true);
-                    //button_b.setBackgroundColor(0xFF673AB7);
+                        button_b.setEnabled(true);
+                        //button_b.setBackgroundColor(0xFF673AB7);
 
-                    button_c.setEnabled(true);
-                    //button_c.setBackgroundColor(0xFF673AB7);
+                        button_c.setEnabled(true);
+                        //button_c.setBackgroundColor(0xFF673AB7);
 
-                    button_d.setEnabled(true);
-                    //button_d.setBackgroundColor(0xFF673AB7);
+                        button_d.setEnabled(true);
+                        //button_d.setBackgroundColor(0xFF673AB7);
+
+                        cdt.start();
+                    }
+
+                    Log.d(TAG, "counter: " + startingindex);
+
                 }
 
             }
         }.start();
 
 
+
     }
 
 
-
+    int countOnce = 0;
     public void addResponse(Button btn, String btnText){
 
         button_a.setEnabled(false);
@@ -303,8 +311,8 @@ public class QuestionActivity extends AppCompatActivity {
 
         int correctWrong;
 
-        Log.d(TAG, "answers: " + answers.get(startingindex-1));
-        if (btnText.equals(answers.get(startingindex-1))){
+        Log.d(TAG, "answers: " + answers.get(startingindex));
+        if (btnText.equals(answers.get(startingindex))){
             correctWrong = 1;
             //Toast.makeText(this, "Correct ans!", Toast.LENGTH_LONG).show();
 
@@ -314,7 +322,7 @@ public class QuestionActivity extends AppCompatActivity {
             //Toast.makeText(this, "Wrong ans!", Toast.LENGTH_LONG).show();
         }
 
-        btn.setBackgroundColor(Color.MAGENTA);
+        btn.setBackgroundColor(getResources().getColor(R.color.selected));
         btnselected = btn; //set global variable for onUpdate();
 
 
@@ -330,7 +338,7 @@ public class QuestionActivity extends AppCompatActivity {
         Map<String, Object> response = new HashMap<>();
         response.put("choice", btnText);
         response.put("correctWrong", correctWrong);
-        response.put("questionNo", question_no.get(startingindex-1));
+        response.put("questionNo", question_no.get(startingindex));
         response.put("responseid", myId);
         response.put("sessionid", value);
         response.put("username", name);
@@ -361,63 +369,80 @@ public class QuestionActivity extends AppCompatActivity {
                 }
 
                 //for displaying correct ans
-                if (button_a.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_a.getText().toString().equals(answers.get(startingindex))){
                     button_a.setClickable(false);
-                    button_a.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_a.setBackgroundColor(Color.parseColor("#A8C256"));
+                    button_a.setTextColor(Color.WHITE);
                 }
                 else {
                     button_a.setBackgroundColor(Color.LTGRAY);
+                    button_a.setTextColor(Color.WHITE);
                     button_a.setEnabled(false);
                 }
-                if (button_b.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_b.getText().toString().equals(answers.get(startingindex))){
                     button_b.setClickable(false);
-                    button_b.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_b.setBackgroundColor(Color.parseColor("#A8C256"));
+                    button_a.setTextColor(Color.WHITE);
                 }
                 else {
                     button_b.setBackgroundColor(Color.LTGRAY);
+                    button_b.setTextColor(Color.WHITE);
                     button_b.setEnabled(false);
                 }
-                if (button_c.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_c.getText().toString().equals(answers.get(startingindex))){
                     button_c.setClickable(false);
-                    button_c.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_c.setBackgroundColor(Color.parseColor("#A8C256"));
+                    button_a.setTextColor(Color.WHITE);
                 }
                 else {
                     button_c.setBackgroundColor(Color.LTGRAY);
+                    button_c.setTextColor(Color.WHITE);
                     button_c.setEnabled(false);
                 }
-                if (button_d.getText().toString().equals(answers.get(startingindex-1))){
+                if (button_d.getText().toString().equals(answers.get(startingindex))){
                     button_d.setClickable(false);
-                    button_d.setBackgroundColor(Color.parseColor("#3D8C40"));
+                    button_d.setBackgroundColor(Color.parseColor("#A8C256"));
+                    button_a.setTextColor(Color.WHITE);
                 }
                 else {
                     button_d.setBackgroundColor(Color.LTGRAY);
+                    button_d.setTextColor(Color.WHITE);
                     button_d.setEnabled(false);
                 }
 
 
                 //for displaying user's answer
-                if (btn.getText().toString().equals(answers.get(startingindex-1))){
-                    btn.setBackgroundColor(Color.GREEN);
+                if (btn.getText().toString().equals(answers.get(startingindex))){
+                    btn.setBackgroundColor(Color.parseColor("#A8C256"));
                     //Toast.makeText(this, "Correct ans!", Toast.LENGTH_LONG).show();
 
                 }
                 else {
-                    btn.setBackgroundColor(Color.RED);
+                    btn.setBackgroundColor(Color.parseColor("#EF5F5F"));
                     //Toast.makeText(this, "Wrong ans!", Toast.LENGTH_LONG).show();
                 }
 
-//                button_a.setEnabled(false);
-//                button_b.setEnabled(false);
-//                button_c.setEnabled(false);
-//                button_d.setEnabled(false);
+
             }
             public void onFinish() {
                 if (startingindex < iterations){
-                    startingindex++;
+                    if (countOnce == 0){
+                        startingindex++;
+                        countOnce = 1;
+                        isSelected = 0;
 
-                    isSelected = 0;
 
-                    cdt.start();
+                        button_a.setClickable(true);
+                        button_b.setClickable(true);
+                        button_c.setClickable(true);
+                        button_d.setClickable(true);
+
+                        cdt.start();
+                    }
+
+//                    isSelected = 0;
+//
+//                    cdt.start();
 
                 }
             }
